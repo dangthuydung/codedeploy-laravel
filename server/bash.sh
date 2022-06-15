7743b7e79cb8178c2c4ad2fa2cf5d952cbf9750c
Dang Thuy Dung <dangthuydung.1909@gmail.com>
2022-06-15 13:38:50 +0700

    push env -nginx

--- server/bash.sh
+++ server/bash.sh
@@ -3,10 +3,10 @@ sudo apt -y update
 sudo apt install -y nginx
 sudo systemctl start nginx
 nginx -v
-sudo apt install mysql-server 
+sudo apt install -y mysql-server 
 sudo mysql
-sudo apt install php-fpm php-mysql 
-sudo apt install php-cli unzip 
+sudo apt install -y php-fpm php-mysql 
+sudo apt install -y php-cli unzip 
 cd ~
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php 
 HASH=`curl -sS https://composer.github.io/installer.sig` 
@@ -14,7 +14,7 @@ echo $HASH
 php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" 
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer 
 composer
-sudo apt install php-mbstring php-xml php-bcmath
+sudo apt install -y php-mbstring php-xml php-bcmath 
 composer create-project --prefer-dist laravel/laravel danhsach
 cd danhsach
 php artisan 
@@ -23,8 +23,7 @@ sudo mv ~/danhsach /var/www/danhsach
 sudo chown -R www-data.www-data /var/www/danhsach/storage 
 sudo chown -R www-data.www-data /var/www/danhsach/bootstrap/cache 
 aws s3 cp s3://my-tf-test-bucket112/nginx.txt /etc/nginx/sites-enabled/danhsach
-sudo ln -s /etc/nginx/sites-available/danhsach/etc/nginx/sites-enabled/ 
-sudo nginx -t 
+sudo ln -s /etc/nginx/sites-available/danhsach /etc/nginx/sites-enabled/ 
 sudo systemctl reload nginx 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 sudo cp /etc/ssl/private/nginx-selfsigned.key /etc/nginx/snippets/self-signed.conf
